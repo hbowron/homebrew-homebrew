eddcb709427ff49f10e212fd2be4691d89ed8bfa
Andy Fleener <andrew.fleener@sportngin.com>
2014-02-26 11:43:10 -0600

    Update brew-gem to point at the new release

--- sportngin-brew-gem.rb
+++ sportngin-brew-gem.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class SportnginBrewGem < Formula
   homepage 'https://github.com/sportngin/brew-gem'
-  head 'https://github.com/sportngin/brew-gem/archive/master.tar.gz'
+  url 'https://github.com/sportngin/brew-gem/archive/v0.2.0.tar.gz'
+  sha1 '94da3eb30de4a94769ab8fdafd9d8db14ce6743b'
 
   def install
     bin.install 'bin/brew-gem'
