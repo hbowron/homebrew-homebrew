b8a30c34bb83ae71f4c76eed32277cbcd32ec5be
Nick LaMuro <nicklamuro@gmail.com>
2014-09-10 18:06:57 -0500

    Update to sportngin-brew-gem v0.5.1

--- sportngin-brew-gem.rb
+++ sportngin-brew-gem.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class SportnginBrewGem < Formula
   homepage 'https://github.com/sportngin/brew-gem'
-  url 'https://github.com/sportngin/brew-gem/archive/v0.5.0.tar.gz'
-  sha1 'd3d714164007301bc15b3eb6f1e24982f6e1ac9b'
+  url 'https://github.com/sportngin/brew-gem/archive/v0.5.1.tar.gz'
+  sha1 '37b77607119f9ca95a7d28c231bb4f3bb3fe1c3f'
 
   def install
     bin.install 'bin/brew-gem'
