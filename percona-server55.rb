d464dbb7626ab3f4a851133d98a8bbc329ab3b0d
Nick Sieger <nick@nicksieger.com>
2017-05-18 12:36:58 -0500

    Sierra bottle for percona 5.5

--- percona-server55.rb
+++ percona-server55.rb
@@ -10,7 +10,9 @@ class PerconaServer55 < Formula
 
   bottle do
     root_url "https://s3.amazonaws.com/sportngin-homebrew-bottles"
-    sha256 "1202b15ad2add10daee1b22cd92af0fb0e27847d965641cf5c156a5d343519a5" => :el_capitan
+    rebuild 1
+    # sha256 "1202b15ad2add10daee1b22cd92af0fb0e27847d965641cf5c156a5d343519a5" => :el_capitan
+    sha256 "5aff4291cd37abe768efcbc4857bf3d0117aafd0e878251e4aef12a3d63eab89" => :sierra
   end
 
   option :universal
